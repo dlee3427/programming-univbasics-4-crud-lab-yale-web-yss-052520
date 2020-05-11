b772140a10b62e3f7f725872cf01914d08e9cb2e
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 19:12:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,6 @@ end
 def remove_element_from_end_of_array(array)
   array = ["Michael Jordan", "Lebron James", "Kareem Abdul-Jabaar", "Wilt Chamberlain", "wow"]
   magic_johnson = array.shift
-  puts array 
 end
 
 def remove_element_from_start_of_array(array)
