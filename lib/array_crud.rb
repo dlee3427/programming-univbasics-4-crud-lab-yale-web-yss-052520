2d717e6f906463415494a69d2e707c133ab8975d
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 19:25:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["Michael Jordan", "Lebron James", "Kareem Abdul-Jabaar", "Wilt Chamberlain"]
-  array
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
