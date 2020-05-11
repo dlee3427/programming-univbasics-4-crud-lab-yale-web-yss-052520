d6765deebfab9768f4a05b709914b7259db404e5
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 21:04:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,9 +34,9 @@ def retrieve_element_from_index(array, index_number)
   p array[2]
 end
 
-
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  p array[0]
 end
 
 def retrieve_last_element_from_array(array)
