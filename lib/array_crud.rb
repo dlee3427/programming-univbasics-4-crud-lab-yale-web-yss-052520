e004cbeb99266b4cad0af86d2fc40cde4fb04e35
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 18:35:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  ["Michael Jordan", "Lebron James", "Kareem Abdul-Jabaar", "Wilt Chamberlain"]
 end
 
 def add_element_to_end_of_array(array, element)
