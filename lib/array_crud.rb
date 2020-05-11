31ce8519bfe0639d92f4dc75dc1e76c4a3947bed
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 18:45:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,6 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  array = ["Michael Jordan", "Lebron James", "Kareem Abdul-Jabaar", "Wilt Chamberlain"]
   array << "Magic Johnson"
 end
 
