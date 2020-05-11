34543f11bf68aba9e082c59b7747b4f7d957b4b1
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 18:43:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array.push("Magic Johnson")
+  create_an_array << "Magic Johnson"
 end
 
 def add_element_to_start_of_array(array, element)
