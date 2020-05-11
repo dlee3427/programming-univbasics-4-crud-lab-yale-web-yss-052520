96f748df750f062b68688401e042ada98362effc
David Lee <david.lee.sl2634@yale.edu>
2020-05-11 18:48:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Michael Jordan", "Lebron James", "Kareem Abdul-Jabaar", "Wilt Chamberlain"]
-  array.push("Magic Johnson")
+  create_an_array = ["Michael Jordan", "Lebron James", "Kareem Abdul-Jabaar", "Wilt Chamberlain"]
+  create_an_array.push("Magic Johnson")
 end
 
 def add_element_to_start_of_array(array, element)
